3840625904f60d61a67814ed71568ca5e049b91f
operatorIO-stock <vova.chigorin@yandex.com>
2024-12-05 16:21:30 +0300

    commit G

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace DegreeOfTwo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberLowerLimit = 2;
+        int numberUpperLimit = 100;
+        int resultOperation = 1;
+        int numberBase = 2;
+        int numberDegree = 0;
+        
+        Random random = new Random();
+        int number = random.Next(numberLowerLimit, numberUpperLimit);
+
+        while (resultOperation <= number)
+        {
+            resultOperation *= numberBase;
+            numberDegree++;
+        }
+        
+        Console.WriteLine($"Number: {number}; Degree: {numberDegree}; Result: {resultOperation}");
+    }
+}
